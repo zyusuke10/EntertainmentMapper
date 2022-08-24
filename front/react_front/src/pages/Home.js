6850736cb8a372a084b33f0c85dffceeac0ca2bf
zyusuke <96976665+zyusuke10@users.noreply.github.com>
2022-08-24 21:47:25 +0900

    バックエンド連携中断

--- front/react_front/src/pages/Home.js
+++ front/react_front/src/pages/Home.js
@@ -1,11 +1,22 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
 import MenuIcon from "@mui/icons-material/Menu";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import Map from "../components/Map";
 import "./Home.css";
 import { EventItem } from "../components/EventItem";
+import axios from "axios";
 
 const Home = () => {
+  
+  const fetchData = async () => {
+    const { data } = await axios.get("http://localhost:8000/api/spot_search");
+    const { name, address, start_date } = data;
+    console.log(name);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <Fragment>
