13a35cc92a15371f657e1962d9b1d1610106fd98
zyusuke <96976665+zyusuke10@users.noreply.github.com>
2022-08-24 17:39:46 +0900

    Mapページデザインの作成

--- front/react_front/src/App.js
+++ front/react_front/src/App.js
@@ -1,8 +1,20 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
 
 export const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 };
 
 export default App;
---
+++ front/react_front/src/pages/Home.js
@@ -0,0 +1,44 @@
+import React, { Fragment } from "react";
+import MenuIcon from "@mui/icons-material/Menu";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import Map from "../components/Map";
+import "./Home.css";
+import { EventItem } from "../components/EventItem";
+
+const Home = () => {
+
+  return (
+    <Fragment>
+      <header>
+        <div className="title">
+          <h2>イベントマップ</h2>
+          <MenuIcon className="menu-icon" />
+        </div>
+      </header>
+
+      <div className="map-container">
+        <div className="event-search-container">
+          <input
+            className="event-search"
+            type="text"
+            name="event"
+            placeholder="イベント名検索"
+          />
+          <ArrowDropDownIcon className="filter-icon" />
+        </div>
+        <div className="map-box">
+          <Map />
+        </div>
+      </div>
+
+      <div className="eventList-container">
+        <div className="event-list-title">
+          <h2>イベント一覧</h2>
+        </div>
+        <EventItem />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Home;
