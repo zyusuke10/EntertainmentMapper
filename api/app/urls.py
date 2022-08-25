1d1c7c71a3f91003274e7d3a8260313e29c22d02
Ryutaro-A <ryu1104.as@gmail.com>
2022-08-25 20:18:09 +0900

    [backend]新規登録・ログイン機能の実装

--- api/app/urls.py
+++ api/app/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('api/spot_search/', views.SpotListCreate.as_view() ),
     path('api/nearby_spot/', views.NearBySpotListCreate.as_view() ),
     path('api/favorite_spot/', views.FavoriteSpotListCreate.as_view() ),
-    path('register/', CreateUserView.as_view(), name='register'),  # 追加
-    path('auth/', include('djoser.urls.jwt')),  # 追加
+    path('api/register/', CreateUserView.as_view(), name='register'),  # 追加
+    path('api/auth/', include('djoser.urls')),  # 追加
+    path('api/auth/', include('djoser.urls.jwt')),  # 追加
 ]
